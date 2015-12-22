5e98b73e76a1a084743b5c2b485854e0bb27a9e9
Darren Loasby <darren.loasby@gmail.com>
2015-12-22 17:29:18 +1100

    Update campaign.rb

--- lib/zuck/facebook/campaign.rb
+++ lib/zuck/facebook/campaign.rb
@@ -20,7 +20,7 @@ module Zuck
 
     parent_object :ad_account, as: :account_id
     list_path :campaigns
-    connections  :ad_sets, :insights, :ad_groups #, :stats
+    connections  :ad_sets, :insights, :ad_groups, :ads #, :stats
 
     # allowed update params:
     #   name
