b38cc4042e25c7470d12964be22c1bb0d2a6caa8
Darren Loasby <darren.loasby@gmail.com>
2015-12-22 18:09:29 +1100

    Update ad.rb

--- lib/zuck/facebook/ad.rb
+++ lib/zuck/facebook/ad.rb
@@ -2,19 +2,28 @@
 module Zuck
   class Ad < RawFbObject
 
-    known_keys adset_id,
-    date_format,
-    creative,
-    name,
-    status,
-    tracking_specs,
-    display_sequence,
-    execution_options,
-    adlabels,
-    bid_amount,
-    redownload
+    known_keys :id,
+      :account_id,        #Ad Account ID
+      :adset_id,          #Ad Set ID
+      # :campaign_group_id, #deprecated on read Campaign ID
+      :campaign_id,      #Campaign ID
+      :adlabels,
+      :date_format,
+      :creative,
+      :name,
+      :status,
+      :tracking_specs,
+      :display_sequence,
+      :execution_options,
+      :adlabels,
+      :bid_amount
     
-    parent_object :ad_campaign
+    parent_object :ad_set, as: :adset_id
+
+    # connections :, #Creative associated with this ad
+    #   :insights,     #A Statistics result for an Ad object
+    #   :previews      #Preview of the ad
+
     list_path     :ads
 
   end
